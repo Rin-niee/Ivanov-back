6c00c2729fc57b7be4e3b97aac284e056231dfb4
Alena Gordienko <127589536+ElenaGordienko1@users.noreply.github.com>
2025-06-20 19:21:38 +1000

    исправила тень у шапки

--- static/js/headerScroll.js
+++ static/js/headerScroll.js
@@ -3,9 +3,12 @@ const header = document.getElementById('header');
 window.addEventListener('scroll', () => {
   const currentScroll = window.scrollY;
 
+  // Тень — убираем, если на самом верху
   if (currentScroll === 0) {
-    header.classList.add('hide');
+    header.classList.add('no-shadow');
+    header.classList.add('hide'); // скрываем, если нужно
   } else {
-    header.classList.remove('hide');
+    header.classList.remove('no-shadow');
+    header.classList.remove('hide'); // показываем при скролле вниз
   }
 });
