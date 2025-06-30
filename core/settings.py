77ca98f16567cd1ea422d8f122352d9f7161e143
Rin <she1111eyyyyyy@gmail.com>
2025-06-30 13:47:28 +1000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-bo6mc=0p+-d(^^ip60i+w56_$aes%=04h!2kh_=x7daacbf^!_'
 
-# # SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
 
+DEBUG = True
 ALLOWED_HOSTS = ['217.25.88.45', '127.0.0.1']
 
 
